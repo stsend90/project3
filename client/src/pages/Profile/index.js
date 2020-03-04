d75e294043718aac62bee3b194bcd180b9f86f4b
Jay\^.^/ <stsend90@yahoo.com>
2020-03-04 00:43:55 -0500

    fix the conflict

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
 import { Container, Row } from "../../components/Grid";
 import NavigationBar from "../../components/navbar";
-<<<<<<< HEAD
-import API from "../../utils/API"
-import Posts from '../../components/PostCard/Posts'
-=======
-import { InputGroup, FormControl, ListGroup } from "react-bootstrap"
-import Post from "./Post";
+import { ListGroup } from "react-bootstrap";
+import Posts from '../../components/PostCard/Posts';
 import API from "../../utils/API";
 
 
 
->>>>>>> 1872154d252773552ce9cc00eb6427c136370a9b
 export default class Profile extends Component {
   constructor(props) {
     super(props);
@@ -53,11 +48,7 @@ export default class Profile extends Component {
           discussions: myDiscussion
         })
 
-<<<<<<< HEAD
-        console.log(this.state.discussions)
-=======
         console.log(this.state.discussion)
->>>>>>> 1872154d252773552ce9cc00eb6427c136370a9b
       })
       .catch(err => console.log(err))
   }
@@ -70,22 +61,20 @@ export default class Profile extends Component {
       return <p>Sorry, no discussions to display!</p>
     }
   }
-
+ 
   render() {
     return (
       <div>
         <Container fluid>
           <NavigationBar logout={this.props.logout} />
-          <br />
-<<<<<<< HEAD
-          <h1>Hello</h1>
           
-=======
-          <h1>{this.state.username}</h1>
-          <br />
-          {this.state.discussions}
-          <br />
->>>>>>> 1872154d252773552ce9cc00eb6427c136370a9b
+          <ListGroup.Item>
+            <br />
+            <h1>{this.state.username}</h1>
+            <br />
+            {this.postCards}
+            <br />
+          </ListGroup.Item>
         </Container>
       </div>
     );
