3d2abcb020e26f36b480cc006e69eb7bb234f258
Victor Dominguez <vd071090@gmail.com>
2020-03-11 19:00:04 -0400

    delete article button working

--- client/src/pages/Profile/savedArticle.js
+++ client/src/pages/Profile/savedArticle.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 
-const SavedArticle = ({article}) => (
+const SavedArticle = ({article, removeSavedArticles}) => (
         <Card border="dark" style={{ width: '18rem' }}>
             <Card.Body>
             <Card.Img variant="top" src={article.image} />
@@ -10,6 +11,7 @@ const SavedArticle = ({article}) => (
                     <h3>Title:</h3>
                     <h3><a href={article.url} target='_blank'>{article.title}</a></h3>
                 </Card.Title>
+                <Button variant="primary" onClick={() => removeSavedArticles(article._id) }>Remove Article</Button>
             </Card.Body>
         </Card>
     )
